43997a95323a462baa8adf57485f3bdf8c8fb781
Andrew Harvard <andrew.t.harvard@gmail.com>
2020-05-28 09:41:47 -0500

    env vars

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,14 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
+    const { DATA_CONTEXT, CONTEXT } = process.env;
 
-  eleventyConfig.addPassthroughCopy("src/css");
-  eleventyConfig.addPassthroughCopy("src/images");
-  eleventyConfig.addPassthroughCopy("src/js");
+    eleventyConfig.addPassthroughCopy("src/css");
+    eleventyConfig.addPassthroughCopy("src/images");
+    eleventyConfig.addPassthroughCopy("src/js");
 
-  return {
-    dir: {
-      input: "src",
-      data: "../data"
-    }
-  };
-
+    return {
+        dir: {
+            input: "src",
+            data: `../data/${DATA_CONTEXT}`,
+        },
+    };
 };
